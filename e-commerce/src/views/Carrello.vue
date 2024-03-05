435ca8198456c94a6d10f49fb1b94c5d1e7c62a7
giuserai99 <giusepperaineri68@gmail.com>
2024-03-06 00:05:42 +0100

    Aggiornato metodo aggiungi carrello(non ancora completato).  Iniziata pagina carrello

--- e-commerce/src/views/Carrello.vue
+++ e-commerce/src/views/Carrello.vue
@@ -3,7 +3,8 @@ import carrelloData from '../assets/carrello.json'
 export default {
     data() {
         return{
-            products: carrelloData
+            products: carrelloData,
+            totale: ''
         }
         
     }
@@ -12,6 +13,15 @@ export default {
 
 <template>
     <main class="bg-[#0a1128] w-full h-screen p-[24px]">
-        <p v-for="product in products" class="text-white">{{ product.name }}</p>
+        <h1 class="text-white font-semibold text-[30px] mb-[30px]">Carrello</h1>
+        <ul>
+            <li v-for="product in products" class="border-2 border-white w-[700px] h-[250px] mb-[20px] p-[24px] flex items-center">
+                <img :src="product.image" :alt="product.title" class="object-small-size h-[200px] mr-[20px]">
+                <div class="flex flex-col items-center">
+                    <span class="text-white">{{ product.title }}</span>
+                    <span class="text-white">{{ product.price }} €</span>
+                </div>
+            </li>
+        </ul>
     </main>
 </template>
