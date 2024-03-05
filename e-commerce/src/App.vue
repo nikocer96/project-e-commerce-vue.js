d3e49ddddcad7ff39817294ae43531ade115f52c
NotAlex575 <dapontealessandro89@gmail.com>
2024-03-05 15:36:15 +0100

    creato metodo login/register con entrata homepage se loggato con successo

--- e-commerce/src/App.vue
+++ e-commerce/src/App.vue
@@ -3,11 +3,14 @@
 import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Login from './views/Login.vue';
 
 </script>
 
 <template>
-  <Header />
+  <Header class="flex justify-end items-end">
+      <router-link class="mr-[20px]" to="./Login">Login</router-link>
+  </Header>
  <RouterView />
  <Footer />
 </template>
---
+++ e-commerce/src/views/Login.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="border border-black h-[400px]">
+        <h1 class="flex justify-center items-center mb-[100px]">Pagina Registrati</h1>
+        <div class="flex flex-col justify-center items-center">
+            <input class="w-[300px] border-2 border-black mb-[20px] text-center" type ="text" v-model="email" placeholder="Registra la Email" />
+            <input class="w-[300px] border-2 border-black mb-[20px] text-center" type ="password" v-model="password" placeholder="Registra la Password" />
+        </div>
+        <div class="flex justify-between items-center mx-[350px] mt-[70px]">
+            <button class="border-2 w-[70px] border-black rounded-full" v-on:click="Login">Login</button>
+            <button class="border-2 w-[90px] border-black rounded-full" v-on:click="SignUp">Registrati</button>
+        </div>
+    </div>
+    </template>
+    
+    <script>
+import { RouterLink } from 'vue-router';
+
+    export default {
+    data()
+    {
+        return {
+            email:"",
+            password:"",
+            emailREGISTRATE:["Alessandro@test.it", "Jesus@test.it", "Giuseppe@test.it", "Nico@test.it"],
+            passwordREGISTRATE:["Test123", "Test123", "Test123", "Test123"]
+        }
+    },
+    methods:{
+         SignUp(){
+            console.log(this.email);
+            console.log(this.password);
+
+                alert("SIGNED UP!");
+                this.emailREGISTRATE.push(this.email);
+                this.passwordREGISTRATE.push(this.password);
+                console.log(this.emailREGISTRATE)
+                console.log(this.passwordREGISTRATE)
+        },
+        Login(){
+            let valuetrovata = false;
+            for(let i = 0; i < this.emailREGISTRATE.length; i++)
+            {
+                if(this.email == this.emailREGISTRATE[i] && this.password == this.passwordREGISTRATE[i])
+                {
+                    valuetrovata = true;
+                    break;
+                }
+                else
+                {
+                    valuetrovata = false;
+                }
+            }
+            if(valuetrovata)
+            {
+                alert("login effettuato con successo!");
+                this.$router.push("./");
+            }
+            else
+            {
+                alert("email o password non corrette! se non sei ancora registrato, clicca su registrati!");
+            }
+        }
+    }
+}
+</script>
+    
+
+
+
+
+
