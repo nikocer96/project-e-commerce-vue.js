3162696b65d76f8dc26009da99bac5805789d65d
giuliacalab <giulia0998@outlook.it>
2024-03-05 16:47:33 +0100

    modifica1

--- e-commerce/src/App.vue
+++ e-commerce/src/App.vue
@@ -4,6 +4,14 @@ import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 
+export default{
+  components: {
+    Header, 
+    Footer
+  }
+
+}
+
 </script>
 
 <template>
--- e-commerce/src/components/Footer.vue
+++ e-commerce/src/components/Footer.vue
@@ -1,8 +1,43 @@
 <script>
 
+
 </script>
 
 
 <template>
+    <!-- <footer class=""> 
+        <div id="logo"> 
+            <img  class=" w-[200px] h-[200px]" src="@/assets/logo.png" alt="logo">
+        </div>
+        <div></div>
+    </footer> -->
+
+    <footer class="bg-white rounded-lg shadow-[#3a1818]  m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="@/assets/logo.png" class=" w-[200px] h[200px]" alt=" Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0 dark:text-black ">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-black">© 2024 <a href= "" class="hover:underline">Room4Commerce™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+    
     
 </template>
