d4272eba24de583ebb354e0a0b91146162360124
giuserai99 <giusepperaineri68@gmail.com>
2024-03-05 12:25:11 +0100

    Aggiunta sezione in evidenza

--- e-commerce/src/views/Home.vue
+++ e-commerce/src/views/Home.vue
@@ -4,38 +4,64 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      prodotti: []
+      prodotti: [],
+      singolo: '',
+      casuale: ''
     }
   },
+  created() {
+    this.casuale = Math.floor(Math.random() * 20) + 1;  
+
+    axios.get(`https://fakestoreapi.com/products/${this.casuale}`)
+      .then(response => {
+        this.singolo = response.data;
+      })
+      .catch (error => {
+        console.error('Errore durante la chiamata API:',error);
+      }) 
+  },
   mounted() {
+    
+
     axios.get('https://fakestoreapi.com/products')
       .then(response => {
         this.prodotti = response.data;
       })
       .catch(error => {
         console.error('Errore durante la chiamata API:',error);
-      })
+      });
   },
   methods: {
     // aggiungiCarrello() {
       
-    // },
-    
+    // }
   }
 
 }
 </script>
 
 <template>
-  <main class="bg-[#0a1128] w-full h-full">
+  <main class="bg-[#0a1128] w-full h-full p-[24px]">
+    <!-- sezione in evidenza -->
+    <section id="jumbotron">
+      <h3 class="text-white font-semibold text-[20px]">Prodotto in evidenza</h3>
+      <div class="mx-auto max-w-[1000px] h-[400px] flex items-center justify-center">
+        <img :src="this.singolo.image" :alt="this.singolo.title" class="object-down-size h-[300px] rounded-2xl">
+        <!-- <span class="text-white">{{ singolo.image }}</span> -->
+        <div class="flex flex-col items-center">
+          <span class="ml-[20px] mb-[20px] text-white">{{ this.singolo.title }}</span>
+          <span class="text-white font-semibold text-[20px]">{{ this.singolo.price }}€</span>
+        </div>
+      </div>
+    </section>
     <!-- Sezione prodotti -->
-    <section class="p-[24px]">
-      <h1 class="text-white">lista dei prodotti</h1>
+    <section>
+      <h3 class="text-white font-semibold text-[20px] mb-[20px]">I nostri prodotti:</h3>
       <ul class="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
         <li v-for="prodotto in prodotti">
           <div class="bg-[#fefcfb] border-2 rounded-2xl flex flex-col items-center justify-center p-[8px]">
             <!-- TODO: al click immagine vai nella pagina del prodotto -->
-            <img :src="prodotto.image" :alt="prodotto.title" class="object-size-down h-[150px] md:h-[200px] lg:h[350px]">
+            <img :src="prodotto.image" :alt="prodotto.title" @click="" class="object-size-down h-[150px] md:h-[200px] lg:h[350px]">
             <span class="mt-[10px] text-black font-medium">{{ prodotto.title }}</span>
             <div class="mt-[10px] flex">
               <span class="text-black font-bold mr-[50px]">{{ prodotto.price }}€</span>
