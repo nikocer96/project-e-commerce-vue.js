f3330261598dbcffeccb06b435d0289f7f527d5d
giuliacalab <giulia0998@outlook.it>
2024-03-06 11:13:45 +0100

    m

--- e-commerce/src/views/AboutUs.vue
+++ e-commerce/src/views/AboutUs.vue
@@ -41,15 +41,15 @@ export default {
 </script>
 
 <template>
-        <div class="bg-[#0a1128]">
-            <h1 class="text-center font-bold text-purple-600 text-5xl pb-9">CHI SIAMO?</h1>
+        <div class="bg-[#FEFDFF]">
+            <h1 class="text-center [text-shadow:0.5px_0.5px_9px_var(--tw-shadow-color)] shadow-[#B02E0C] font-anta font-bold text-black text-5xl pb-9">CHI SIAMO?</h1>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-10 ">
                 <div v-for="item in sviluppatori" class="flex justify-around items-center md:flex-col">
                     <!-- <span class="w-[40px] md:hidden">{{ item.nome }}</span> -->
                     <img class="md:h-auto md:max-w-full rounded-lg h-[200px] w-[250px]" :src="item.immagine" :alt="item.nome">
                     <div class="flex justify-center items-center flex-col pl-2">
-                        <span class="max-md:w-[40px] text-white pb-3">{{ item.nome }}</span>
-                        <span class="max-md:w-[40px] text-white">{{ item.lavoro }}</span>
+                        <span class="max-md:w-[40px] text-[#B02E0C] pb-3">{{ item.nome }}</span>
+                        <span class="max-md:w-[40px] text-[#B02E0C]">{{ item.lavoro }}</span>
                 </div>    
             </div>
         </div>    
--- e-commerce/src/views/Login.vue
+++ e-commerce/src/views/Login.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="border border-black h-[400px] bg-[#0a1128]">
-        <h1 class="flex justify-center items-center mb-[100px] text-white">Pagina Login/Registrati</h1>
+    <div class="border border-[#EB4511] h-[400px] bg-[#C1BFB5]">
+        <h1 class="flex justify-center items-center mb-[100px] text-[#B02E0C]">Pagina Login/Registrati</h1>
         <div class="flex flex-col justify-center items-center">
-            <input class="w-[300px] border-2 border-black mb-[20px] text-center text-white" type ="text" v-model="email" placeholder="Registra la Email" />
-            <input class="w-[300px] border-2 border-black mb-[20px] text-center text-white" type ="password" v-model="password" placeholder="Registra la Password" />
+            <input class="w-[300px] border-2 border-[#EB4511] mb-[20px] text-center text-[#B02E0C]" type ="text" v-model="email" placeholder="Registra la Email" />
+            <input class="w-[300px] border-2 border-[#EB4511] mb-[20px] text-center text-[#B02E0C]" type ="password" v-model="password" placeholder="Registra la Password" />
         </div>
         <div class="flex justify-between items-center mx-[350px] mt-[70px]">
-            <button class="border-2 w-[70px] border-white rounded-full text-white" v-on:click="Login">Login</button>
-            <button class="border-2 w-[90px] border-white rounded-full text-white" v-on:click="SignUp">Registrati</button>
+            <button class="border-2 w-[70px] border-[#EB4511] rounded-full text-[#B02E0C]" v-on:click="Login">Login</button>
+            <button class="border-2 w-[90px] border-[#EB4511] rounded-full text-[#B02E0C] font-anta-regular" v-on:click="SignUp">Registrati</button>
         </div>
     </div>
     </template>
