54020221aa51ef3e5f1565d41f60e2a1035d6318
nikocer96 <nikocerone96@gmail.com>
2024-03-07 15:49:57 +0100

    aggiunto pagamento

--- e-commerce/src/components/Header.vue
+++ e-commerce/src/components/Header.vue
@@ -1,130 +1,106 @@
 <script>
 import { RouterLink } from 'vue-router';
-import axios from 'axios'
-import { computed } from 'vue';
+import axios from 'axios';
 
 export default {
-    props: {
-        isLoggedIn: Boolean
+  props: {
+    isLoggedIn: Boolean,
+  },
+  data() {
+    return {
+      isDropdownOpen: false,
+      selectedOption: null,
+      quantita: 0,
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
     },
-    data() {
-        return {
-            isDropdownOpen: false,
-            selectedOption: null,
-            quantita: 0
-        };
+    handleOptionClick(option) {
+      this.selectedOption = option;
+      this.isDropdownOpen = false;
     },
-    methods: {
-        toggleDropdown() {
-            this.isDropdownOpen = !this.isDropdownOpen;
-        },
-        handleOptionClick(option) {
-            this.selectedOption = option;
-            this.isDropdownOpen = false;
-        },
-        handleLogout() {
-            this.$emit("logout");
-            this.$router.push("./");
-        }
+    async handleLogout() {
+      await this.$emit('logout');
+      this.$router.push('./');
     },
-    async created() {
-        await this.prendiProdotti();
+    async updateQuantita() {
+      const response = await axios.get('http://localhost:3000/prodotti');
+      this.quantita = response.data.length;
+      console.log(this.quantita);
     },
-    computed: {
-        async prendiProdotti() {
-            try{
-                const response = await axios.get('http://localhost:3000/prodotti');
-                this.quantita = response.data.length;
-                console.log(this.quantita);
-                return this.quantita;
-            }catch(error) {
-                console.error("Errore durante la chiamata al json locale:", error)
-            }
-    }
-}
- 
-}
+  },
+  async created() {
+    await this.updateQuantita();
+  },
+};
 </script>
 
-
 <template>
-    <nav class="overflow-x-hidden">
-        <div class="flex h-[100px] bg-[#0a1128] text-white justify-between items-center p-[10px]">
-            <div class="mr-[10px]">
-                <RouterLink to="/">
-                    <img class="h-[60px] w-[70px] md:max-w-none md:h-[100px] md:w-[100px]"
-                        src="../assets/logo5.png" alt="logo-e-commerce">
+  <nav class="overflow-x-hidden">
+    <div class="flex h-[100px] bg-[#0a1128] text-white justify-between items-center p-[10px]">
+      <div class="mr-[10px]">
+        <RouterLink to="/">
+          <img class="h-[60px] w-[70px] md:max-w-none md:h-[100px] md:w-[100px]" src="../assets/logo5.png" alt="logo-e-commerce">
+        </RouterLink>
+      </div>
+      <div class="hidden min-[480px]:flex mr-[20px]">
+        <RouterLink to="/" class="border-[1px] border-white p-[7px] rounded-md text-xl font-serif">Home</RouterLink>
+      </div>
+      <div>
+        <div class="flex justify-center items-center">
+          <div>
+            <ul class="flex">
+              <li>
+                <RouterLink to="/about" class=" text-xl font-serif hover:text-2xl max-[600px]:mr-[10px] ml-[20px] border-[1px] border-white rounded-md p-[7px]">
+                  About us
                 </RouterLink>
+              </li>
+            </ul>
+          </div>
+          <form class="flex justify-end items-center">
+            <div class="hidden max-[600px]:flex">
+              <RouterLink to="#">
+                <img class="h-[30px] w-[30px] mt-[5px] mr-[20px] rounded-sm" src="../assets/lente.svg" alt="immagine-lente-ricerca">
+              </RouterLink>
             </div>
-            <div class="hidden min-[480px]:flex mr-[20px]">
-                <RouterLink to="/" class="border-[1px] border-white p-[7px] rounded-md text-xl font-serif">Home</RouterLink>
+            <input class="hidden min-[600px]:flex w-[100%] h-[100%] ml-[15px] mr-[10px] rounded-[20px]" type="text" placeholder="Ricerca">
+          </form>
+          <div class="hidden min-[730px]:flex justify-center items-center">
+            <div>                        
+              <div v-if="isLoggedIn">                           
+                <button class="border-[1px] border-white rounded-md p-[7px] text-xl font-serif font-semibold" @click="handleLogout">Logout</button>            
+                <RouterLink to="/carrello" class="ml-[10px] border-[1px] border-white rounded-md p-[7px] text-xl font-serif font-semibold" @click="updateQuantita">
+                  Carrello <span class="bg-white w-[30px] h-[30px] text-black text-center rounded-[50%]">{{ quantita }}</span>
+                </RouterLink>
+              </div>                         
+              <div v-else>                             
+                <router-link to="/login" class="border-[1px] border-white rounded-md p-[7px] text-xl font-serif font-semibold">Login</router-link>                         
+              </div>                     
             </div>
-            <div>
-
-                <div class="flex justify-center items-center">
-
-                    <div>
-                        <ul class="flex">
-                            <li><RouterLink to="/about" class=" text-xl font-serif hover:text-2xl max-[600px]:mr-[10px] ml-[20px] border-[1px] border-white rounded-md p-[7px]"
-                                    >About us</RouterLink></li>
-                        </ul>
-                    </div>
-
-                    <form class="flex justify-end items-center">
-                        <div class="hidden max-[600px]:flex">
-                            <RouterLink to="#"><img class="h-[30px] w-[30px] mt-[5px] mr-[20px] rounded-sm"
-                                    src="../assets/lente.svg" alt="immagine-lente-ricerca"></RouterLink>
-                        </div>
-                        <input class="hidden min-[600px]:flex w-[100%] h-[100%] ml-[15px] mr-[10px] rounded-[20px]"
-                            type="text" placeholder="Ricerca">
-                    </form>
-                    <div class="hidden min-[730px]:flex justify-center items-center">
-                        <div>                        
-                            <div v-if="isLoggedIn">                           
-                                <button class="border-[1px] border-white rounded-md p-[7px] text-xl font-serif font-semibold" @click="handleLogout">Logout</button>            
-                                <RouterLink to="/carrello" class="ml-[10px] border-[1px] border-white rounded-md p-[7px] text-xl font-serif font-semibold">Carrello <span class="bg-white w-[30px] h-[30px] text-black text-center rounded-[50%]">{{ quantita }}</span></RouterLink>
-                            </div>                         
-                            <div v-else>                             
-                                <router-link to="/login" class="border-[1px] border-white rounded-md p-[7px] text-xl font-serif font-semibold">Login</router-link>                         
-                            </div>                     
-                        </div>
-                    </div>
-
-                    <div class="hidden max-[730px]:flex">
-                        <button class="bg-blue-500 text-white px-3 py-1 rounded" @click="toggleDropdown">
-                            Opzioni
-                            <svg class="w-3 h-3 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M19 9l-7 7-7-7"></path>
-                            </svg>
-                        </button>
-
-                        <div v-if="isDropdownOpen" class="absolute mt-1 bg-white border rounded-md shadow-md z-20">
-                                                  
-                            <div v-if="isLoggedIn">                           
-                            <button class="w-[80px] h-[30px] border-[1px] border-white rounded-md p-[7px] text-black text-xl font-serif font-semibold" @click="handleLogout">Logout</button> 
-                            <button class="w-[80px] h-[30px] border-[1px] border-white rounded-md p-[7px] text-black text-xl font-serif font-semibold">
-                                <RouterLink to="/carrello">Carrello</RouterLink>
-                            </button>           
-                            </div>                         
-                            <div v-else>                             
-                            <router-link to="/login" class="border-[1px] border-white rounded-md p-[7px] text-xl font-serif font-semibold text-black">Login</router-link>                         
-                            </div>                     
-                        </div>
-                    </div>
-                </div>
+          </div>
+          <div class="hidden max-[730px]:flex">
+            <button class="bg-blue-500 text-white px-3 py-1 rounded" @click="toggleDropdown">
+              Opzioni
+              <svg class="w-3 h-3 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+              </svg>
+            </button>
+            <div v-if="isDropdownOpen" class="absolute mt-1 bg-white border rounded-md shadow-md z-20">
+              <div v-if="isLoggedIn">
+                <button class="w-[80px] h-[30px] border-[1px] border-white rounded-md p-[7px] text-black text-xl font-serif font-semibold" @click="handleLogout">Logout</button>
+                <button class="w-[80px] h-[30px] border-[1px] border-white rounded-md p-[7px] text-black text-xl font-serif font-semibold" @click="updateQuantita">
+                  <RouterLink to="/carrello">Carrello</RouterLink>
+                </button>           
+              </div>                         
+              <div v-else>                             
+                <router-link to="/login" class="border-[1px] border-white rounded-md p-[7px] text-xl font-serif font-semibold text-black">Login</router-link>                         
+              </div>                     
             </div>
+          </div>
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
-
-
-
-
-
-
-
-
-
-
--- e-commerce/src/views/Carrello.vue
+++ e-commerce/src/views/Carrello.vue
@@ -2,34 +2,46 @@
 import axios from 'axios'
 export default {
     data() {
-        return{
+        return {
             products: [],
-            totale: ''
+            totale: '',
+            nome: '',
+            cognome: '',
+            numeroCarta: 0
         }
-        
+
     },
     mounted() {
         this.prendiProdotti();
     },
     methods: {
         async rimuoviProdotto(index) {
-            // const response = await axios.delete(`http://localhost:3000/prodotti/${index}`)
-                // .then(response => this.prendiProdotti());
-                await axios.delete(`http://localhost:3000/prodotti/${index}`);
-                await this.prendiProdotti();
-                this.$emit('prodottoRimosso');
+            await axios.delete(`http://localhost:3000/prodotti/${index}`);
+            await this.prendiProdotti();
+            this.$emit('prodottoRimosso');
         },
         async prendiProdotti() {
-            try{
+            try {
                 const response = await axios.get('http://localhost:3000/prodotti');
-                    this.products = response.data;
-            }catch(error) {
+                this.products = response.data;
+            } catch (error) {
                 console.error("Errore durante la chiamata al json locale:", error)
             }
         },
-        
+        async pagamento() {
+            let risultato = await axios.post(
+                `http://localhost:3000/carteUtenti`, {
+                nome: this.nome,
+                cognome: this.cognome,
+                numeroCarta: this.numeroCarta,
+            }
+            )
+            alert("Pagamento effettuato con successo!")
+            
+            
+        }
     },
-    computed: 
+    computed:
     {
         sommaPrezzi() {
             this.totale = this.products.reduce((acc, product) => acc + product.price, 0);
@@ -44,18 +56,45 @@ export default {
     <main class="bg-[#0a1128] w-full h-full p-[24px] flex justify-around items-center flex-col lg:flex-row">
         <div class="flex flex-col">
             <h1 class="text-white font-semibold text-[30px] mb-[30px]">Carrello</h1>
-        <h3 class="text-white font-semibold text-[30px] text-center mt-[100px] mb-[250px]" v-if="this.products.length===0">Il carrello è vuoto</h3>
-        <ul v-else>
-            <li v-for="product in products" class="border-2 border-white w-[700px] h-[250px] mb-[20px] p-[24px] flex items-center">
-                <img :src="product.image" :alt="product.title" class="object-small-size h-[200px] mr-[20px]">
-                <div class="flex flex-col items-center">
-                    <span class="text-white">{{ product.title }}</span>
-                    <span class="text-white">{{ product.price }} €</span>
-                    <button @click="rimuoviProdotto(product.id)" class="mt-[20px] text-white w-[100px] h-[60px] border-2 rounded-lg">Rimuovi</button>
+            <h3 class="text-white font-semibold text-[30px] text-center mt-[100px] mb-[250px]"
+                v-if="this.products.length === 0">Il carrello è vuoto</h3>
+            <ul v-else>
+                <li v-for="product in products"
+                    class="border-2 border-white w-[700px] h-[250px] mb-[20px] p-[24px] flex items-center">
+                    <img :src="product.image" :alt="product.title" class="object-small-size h-[200px] mr-[20px]">
+                    <div class="flex flex-col items-center">
+                        <span class="text-white">{{ product.title }}</span>
+                        <span class="text-white">{{ product.price }} €</span>
+                        <button @click="rimuoviProdotto(product.id)"
+                            class="mt-[20px] text-white w-[100px] h-[60px] border-2 rounded-lg">Rimuovi</button>
+                    </div>
+                </li>
+            </ul>
+            <div class="">
+                <div class="h-[250px] w-[700px] border-[1px] border-white flex justify-center items-center ">
+                    <form>
+                        <label class="text-white mr-[10px]" for="name">Nome</label>
+                        <input class="rounded-lg mr-[10px]" type="text" v-model="nome" placeholder="inserisci il nome..." required>
+                        <label class="text-white mr-[10px]" for="cognome">Cognome</label>
+                        <input class="rounded-lg" type="text" v-model="cognome" placeholder="inserisci il cognome..." required> <br>
+                        <div class="mt-[30px] flex">
+                            <label class="text-white mr-[10px]" for="numeroCarta">Num. Carta</label>
+                            <input class="mr-[30px]" type="number" v-model="numeroCarta" placeholder="inserisci il numero della carta..."
+                                required> <br>
+                            
+                                
+                                    <button class="text-white border-[1px] border-white p-[10px] pl-[20px] pr-[20px] hover:text-purple-400 rounded-md"
+                                     @click="pagamento">PAGA</button>
+                               
+                        </div>
+                        
+                    </form>
                 </div>
-            </li>
-        </ul>
+            </div>
         </div>
         <div class="text-white mt-4 text-xl font-medium">Totale: {{ sommaPrezzi }} €</div>
+
+
     </main>
+
 </template>
